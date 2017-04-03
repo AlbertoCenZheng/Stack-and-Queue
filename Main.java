79ca7c4ffc393592ff6f07c8d590c630d047274e
Alberto Cen Zheng <AlbertoCenZheng@users.noreply.github.com>
2017-04-02 22:10:49 -0400

    Add files via upload
    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Alberto
+ * 
+ * Stack and Queue;
+ * this project is about creating a stack and queue;
+ * As well as making a method to transfer contents from stack to queue ;
+ * 
+ * CSI 213
+ * Dr.Magenus;
+ * Apr/2/2017
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
