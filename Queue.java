da710bb4f511cbb76ad25cebb0419852822784b2
Alberto Cen Zheng <AlbertoCenZheng@users.noreply.github.com>
2017-04-03 14:02:51 -0400

    Add files via upload
    add enqueue, dequeue, peek;
    add pop, push, peek;

--- Queue.java
+++ Queue.java
@@ -10,4 +10,20 @@
  */
 public class Queue {
 
+	public Node front , end;
+	
+
+	public Node peek(){
+		return end;
+	}
+	
+	public void dequeue(){
+		end = end.getPrev();
+		end.setNext(null);
+	}
+	
+	public void enqueue(Node newnode){
+		newnode.setNext(front);
+		front = newnode;
+	}
 }
--- Stack.java
+++ Stack.java
@@ -9,5 +9,19 @@
  * Apr/2/2017
  */
 public class Stack {
-
+	public Node top;
+	
+	public Node peek(){
+		return top;
+	}
+	
+	public void pop(){
+		top = top.getNext();
+		top = null;
+	}
+	
+	public void push(Node newnode){
+		newnode.setNext(top);
+		top = newnode;
+	}
 }
