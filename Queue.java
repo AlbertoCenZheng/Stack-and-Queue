c32da0d715d51b71a067eeab4251f47cf0f0789d
Alberto Cen Zheng <AlbertoCenZheng@users.noreply.github.com>
2017-04-02 22:13:11 -0400

    Add files via upload
    Initial commit

---
+++ Queue.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Alberto
+ * 
+ * Queue class;
+ * 
+ * CSI 213
+ * Dr.Magenus;
+ * Apr/2/2017
+ */
+public class Queue {
+
+}
---
+++ Stack.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Alberto
+ * 
+ * Stack class;
+ * 
+ * CSI 213
+ * Dr.Magenus;
+ * Apr/2/2017
+ */
+public class Stack {
+
+}
